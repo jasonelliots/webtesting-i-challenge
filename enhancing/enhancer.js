ee92b2993e54de49699a6df1f3d4adc95a0605ab
jasonelliots <j.elliotschwartz@gmail.com>
2020-07-22 13:10:48 -0700

    completed mvp

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,16 +5,42 @@ module.exports = {
   get,
 };
 
+
+// - The item's enhancement increases by 1.
+// - If the item enhancement level is 20, the enhancement level is not changed.
+// - The durability of the item is not changed.
+
 function success(item) {
+  if(item.enhancement < 20){
+    item.enhancement = item.enhancement + 1
+    return { ...item };
+  } else {
   return { ...item };
+  }
 }
 
+// - If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// - If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// - If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+
 function fail(item) {
-  return { ...item };
+  if(item.enhancement < 15){
+    item.durability = item.durability - 5 
+    return { ...item}
+  } else if (item.enhancement >= 15 ){
+    item.durability = item.durability - 10
+      if(item.enhancement > 16){
+        item.enhancement = item.enhancement - 1
+        return { ...item}
+      } else {
+        return { ...item}
+      }}
+ 
 }
 
 function repair(item) {
-  return { ...item };
+  item.durability = 100 
+  return { ...item}
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,96 @@
 const enhancer = require('./enhancer.js');
-// test away!
+
+describe("repair()", function () {
+    it("should repair item's durability to 100", function () {
+      //set up - arrange
+      const item = {
+          name: "hammer",
+          durability: 50,
+          enhancement: 5, 
+      }
+      //execute code under test - act
+      const updatedItem = enhancer.repair(item);
+      // assert
+      expect(updatedItem.durability).toBe(100);
+    });
+});
+
+
+describe("success()", function () {
+    it("should add +1 enhancement", function () {
+      //set up - arrange
+      const item = {
+          name: "hammer",
+          durability: 50,
+          enhancement: 5, 
+      }
+      //execute code under test - act
+      const updatedItem = enhancer.success(item);
+      // assert
+      expect(updatedItem.enhancement).toBe(6);
+    });
+
+    it("should not add +1 if enhancement is already 20", function() {
+        const item = {
+            name: "hammer",
+            durability: 50,
+            enhancement: 20, 
+        }
+        //execute code under test - act
+        const updatedItem = enhancer.success(item);
+        // assert
+        expect(updatedItem.enhancement).toBe(20);
+    })
+
+});
+
+
+
+// - If the item's enhancement is less than 15, the durability of the item is decreased by 5.
+// - If the item's enhancement is 15 or more, the durability of the item is decreased by 10.
+// - If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17).
+
+describe("fail()", function () {
+    it("item's enhancment less than 15, should decrease durability by 5", function () {
+      //set up - arrange
+      const item = {
+          name: "hammer",
+          durability: 50,
+          enhancement: 10, 
+      }
+      //execute code under test - act
+      const updatedItem = enhancer.fail(item);
+      // assert
+      expect(updatedItem.durability).toBe(45);
+    });
+
+    it("item's enhancement more than 14 but less than 17, should decrease durability by 10 and leave enhancement unchanged", function () {
+        //set up - arrange
+        const item = {
+            name: "hammer",
+            durability: 40,
+            enhancement: 16, 
+        }
+        //execute code under test - act
+        const updatedItem = enhancer.fail(item);
+        // assert
+        expect(updatedItem.durability).toBe(30);
+        expect(updatedItem.enhancement).toBe(16);
+      });
+
+    it("item's enhancement is 17 or greater, should decrease durability by 10 AND decrease enhancement by 1", function () {
+        //set up - arrange
+        const item = {
+            name: "hammer",
+            durability: 40,
+            enhancement: 18, 
+        }
+        //execute code under test - act
+        const updatedItem = enhancer.fail(item);
+        // assert
+        expect(updatedItem.durability).toBe(30);
+        expect(updatedItem.enhancement).toBe(17);
+      });
+
+});
+
